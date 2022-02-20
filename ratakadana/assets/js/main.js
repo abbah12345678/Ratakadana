9024a78c14549e06cbabf83f5ef11055bc6d4369
abbah12345678 <abhirukprashan436@gmail.com>
2022-02-20 12:30:46 +0530

    gg

--- ratakadana/assets/js/main.js
+++ ratakadana/assets/js/main.js
@@ -1,19 +1,19 @@
-/*===== MENU SHOW Y HIDDEN =====*/
+
 const navMenu = document.getElementById('nav-menu'),
     toggleMenu = document.getElementById('nav-toggle'),
     closeMenu = document.getElementById('nav-close')
 
-// SHOW
+
 toggleMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('show')
 })
 
-// HIDDEN
+
 closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
 
-/*===== MOUSEMOVE HOME IMG =====*/
+
 document.addEventListener('mousemove', move);
 function move(e){
     this.querySelectorAll('.move').forEach(layer =>{
@@ -26,12 +26,10 @@ function move(e){
     })
 }
 
-/*===== GSAP ANIMATION =====*/
-// NAV
+
 gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
 gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
 
-// HOME
 gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
 gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
 gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
